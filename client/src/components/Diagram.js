084cf53d69f18a67916fe0d9fc52739c192cc0ad
flex1192 <kamil.malysiak@gmail.com>
2020-01-14 22:10:22 +0100

    add 8086 block diagram component

---
+++ client/src/components/Diagram.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import {ReactComponent as DiagramCanva} from '../assets/Intel_8086_block_scheme.svg'
+
+
+export default class Diagram extends Component {
+    render() {
+        return (
+            <div>
+                <DiagramCanva/>
+            </div>
+        )
+    }
+}
