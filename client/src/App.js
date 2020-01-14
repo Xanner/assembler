8cafc26122475bc32a8f13c3db6ae6c798006ded
Arek Klis <arek9513@gmail.com>
2020-01-14 22:42:12 +0100

    Added antd package

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Diagram from './components/Diagram';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
