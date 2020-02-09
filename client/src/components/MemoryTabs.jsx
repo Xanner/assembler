911da2cdcb001ebf5beb81d838e1d9f047e7e223
Mona Lisa <arek9513@gmail.com>
2020-02-09 22:32:44 +0100

    Registers added

--- client/src/components/MemoryTabs.jsx
+++ client/src/components/MemoryTabs.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Card } from "antd";
+import { Card, Input } from "antd";
 
 import { convertToHex } from "./ArithmeticCodeItems";
 
@@ -16,6 +16,10 @@ const tabListNoTitle = [
   {
     key: "stackSegment",
     tab: "Stack Segment"
+  },
+  {
+    key: "registers",
+    tab: "Rejestry"
   }
 ];
 
@@ -51,32 +55,124 @@ const contentListNoTitle = {
     if (index % 16 === 0)
       return (
         <span key={item.address}>
-          <br />
-          <b>{item.address}:</b> {item.content}{" "}
+          {index !== 0 && <br />}
+          <b>{item.address}:</b>{" "}
+          <Input
+            style={{ width: "31px" }}
+            size="small"
+            disabled
+            value={item.content}
+          />
         </span>
       );
-    return <span key={item.address}>{item.content} </span>;
+    return (
+      <span key={item.address}>
+        <Input
+          style={{ width: "31px" }}
+          size="small"
+          disabled
+          value={item.content}
+        />
+      </span>
+    );
   }),
   codeSegment: CSRAM.map((item, index) => {
     if (index % 16 === 0)
       return (
         <span key={item.address}>
-          <br />
-          <b>{item.address}:</b> {item.content}{" "}
+          {index !== 0 && <br />}
+          <b>{item.address}:</b>{" "}
+          <Input
+            style={{ width: "31px" }}
+            size="small"
+            disabled
+            value={item.content}
+          />
         </span>
       );
-    return <span key={item.address}>{item.content} </span>;
+    return (
+      <span key={item.address}>
+        <Input
+          style={{ width: "31px" }}
+          size="small"
+          disabled
+          value={item.content}
+        />
+      </span>
+    );
   }),
   stackSegment: STACKRAM.map((item, index) => {
     if (index % 16 === 0)
       return (
         <span key={item.address}>
-          <br />
-          <b>{item.address}:</b> {item.content}{" "}
+          {index !== 0 && <br />}
+          <b>{item.address}:</b>{" "}
+          <Input
+            style={{ width: "31px" }}
+            size="small"
+            disabled
+            value={item.content}
+          />
         </span>
       );
-    return <span key={item.address}>{item.content} </span>;
-  })
+    return (
+      <span key={item.address}>
+        <Input
+          style={{ width: "31px" }}
+          size="small"
+          disabled
+          value={item.content}
+        />
+      </span>
+    );
+  }),
+  registers: (
+    <>
+      <div>
+        AX <Input style={{ width: "30px" }} size="small" disabled />{" "}
+        <Input style={{ width: "30px" }} size="small" disabled />
+      </div>
+      <div>
+        BX <Input style={{ width: "30px" }} size="small" disabled />{" "}
+        <Input style={{ width: "30px" }} size="small" disabled />
+      </div>
+      <div>
+        CX <Input style={{ width: "30px" }} size="small" disabled />{" "}
+        <Input style={{ width: "30px" }} size="small" disabled />
+      </div>
+      <div>
+        DX <Input style={{ width: "30px" }} size="small" disabled />{" "}
+        <Input style={{ width: "30px" }} size="small" disabled />
+      </div>
+      <div>
+        CS <Input style={{ width: "60px" }} size="small" disabled />
+      </div>
+      <div>
+        IP <Input style={{ width: "60px" }} size="small" disabled />
+      </div>
+      <div>
+        SS <Input style={{ width: "60px" }} size="small" disabled />
+      </div>
+      <div>
+        SP <Input style={{ width: "60px" }} size="small" disabled />
+      </div>
+      <div>
+        BP <Input style={{ width: "60px" }} size="small" disabled />
+      </div>
+      <div>
+        SI <Input style={{ width: "60px" }} size="small" disabled />
+      </div>
+      <div>
+        DI <Input style={{ width: "60px" }} size="small" disabled />
+      </div>
+      <div>
+        DS <Input style={{ width: "60px" }} size="small" disabled />
+      </div>
+      <div>
+        ES <Input style={{ width: "60px" }} size="small" disabled />
+      </div>
+    </>
+  )
 };
 
 export default class TabsCard extends React.Component {
@@ -85,14 +181,13 @@ export default class TabsCard extends React.Component {
   };
 
   onTabChange = key => {
-    console.log(key);
     this.setState({ selectedTab: key });
   };
 
   render() {
     return (
       <Card
-        style={{ width: "100%" }}
+        style={{ width: "100%", textAlign: "center" }}
         tabList={tabListNoTitle}
         activeTabKey={this.state.selectedTab}
         onTabChange={key => {
