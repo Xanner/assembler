2fd185fdbc2b309316775e747a453a6d6c6acb2b
Mona Lisa <arek9513@gmail.com>
2020-02-12 18:08:42 +0100

    division updated

--- client/src/pages/arithmetic/DivisionPage.jsx
+++ client/src/pages/arithmetic/DivisionPage.jsx
@@ -1,16 +1,34 @@
 import React, { PureComponent } from "react";
 import divisionExample from "../../dal/arithmetic/division.json";
 import ArithmeticPage from "./ArithmeticPage";
+import { Row, InputNumber } from "antd";
 
+const divisionAction = (
+  <>
+    <Row style={{ marginBottom: 16 }}>
+      <InputNumber
+        style={{ width: "60px" }}
+        value={3}
+        size="default"
+        disabled
+      />
+      <span style={{ fontSize: 18 }}> : </span>
+      <InputNumber
+        style={{ width: "60px" }}
+        value={5}
+        size="default"
+        disabled
+      />
+    </Row>
+  </>
+);
 export default class MultiplicationPage extends PureComponent {
-  division = (firstNumber, secondNumber) => firstNumber / secondNumber;
-
   render() {
     return (
       <ArithmeticPage
         headerTitle="Operacje arytmetyczne - dzielenie"
         data={divisionExample}
-        arithmeticSign={<span style={{ fontSize: 18 }}> : </span>}
+        userAction={divisionAction}
         cardText="Podziel dwie liczby przez siebie i naciśnij Start."
         handleArithmeticOperation={this.division}
       />
