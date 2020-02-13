d9e9840368dee4cf1a07dd226f847f3130607508
Mona Lisa <arek9513@gmail.com>
2020-02-13 01:01:19 +0100

    cleanup

--- client/src/components/Pdf.jsx
+++ client/src/components/Pdf.jsx
@@ -1,90 +1,99 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Layout, Col, Row, Button, Icon } from "antd";
 
 const { Content, Header } = Layout;
-const pdfjsLib = window['pdfjs-dist/build/pdf'];
+const pdfjsLib = window["pdfjs-dist/build/pdf"];
 
 export default class Pdf extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            path: require("../assets/pdf/Rejestry.pdf"),
-            pdf: null,
-            pageNum: 1,
-            endPage: 1
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      path: this.props.pdfSource,
+      pdf: null,
+      pageNum: 1,
+      endPage: 1
+    };
+  }
+
+  loadPDF = async () => {
+    const pdf = await pdfjsLib.getDocument(this.state.path);
+    await this.setState({ pdf: pdf, endPage: pdf.numPages });
+    this.renderPage(1);
+  };
+  renderPage = pageNum => {
+    const { pdf } = this.state;
+    pdf.getPage(pageNum).then(page => {
+      const canvas = document.querySelector(".pdf-window");
+      var viewport = page.getViewport({ scale: 1.2 });
+      var context = canvas.getContext("2d");
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+      var renderContext = {
+        canvasContext: context,
+        viewport: viewport
+      };
+      var renderTask = page.render(renderContext);
+      renderTask.promise.then(function() {
+        console.log("Page rendered");
+      });
+    });
+  };
+  changePage = offset => {
+    const { pageNum, endPage } = this.state;
+    const newPage =
+      pageNum + offset < 1
+        ? 1
+        : pageNum + offset > endPage
+        ? endPage
+        : pageNum + offset;
+    if (newPage !== pageNum) {
+      this.renderPage(newPage);
+      this.setState({ pageNum: newPage });
     }
+  };
 
-    loadPDF = async () => {
-        const pdf = await pdfjsLib.getDocument(this.state.path)
-        await this.setState({ pdf: pdf, endPage: pdf.numPages })
-        this.renderPage(1);
+  componentDidMount() {
+    this.loadPDF();
+  }
+  render() {
+    return (
+      <>
+        <Header style={{ background: "#fff" }}>
+          <h2>{this.props.title}</h2>
+        </Header>
+        <Content>
+          <Row type="flex" justify={"center"}>
+            <Col xs={7}>
+              <canvas className="pdf-window" />
+            </Col>
+          </Row>
+          <Row type="flex" justify={"center"}>
+            <Col xs={7}>
+              <Row type="flex" justify={"center"}>
+                <Col xs={12}>
+                  <Button
+                    type="primary"
+                    onClick={() => this.changePage(-1)}
+                    icon="left"
+                  >
+                    Poprzednia
+                  </Button>
+                </Col>
 
-    }
-    renderPage = pageNum => {
-        const { pdf } = this.state
-        pdf.getPage(pageNum).then(page => {
-            const canvas = document.querySelector('.pdf-window');
-            var viewport = page.getViewport({ scale: 1.2 });
-            var context = canvas.getContext('2d');
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
-            var renderContext = {
-                canvasContext: context,
-                viewport: viewport
-            };
-            var renderTask = page.render(renderContext);
-            renderTask.promise.then(function () {
-                console.log('Page rendered');
-            });
-        })
-    }
-    changePage = offset => {
-        const { pageNum, endPage } = this.state;
-        const newPage = pageNum + offset < 1 ? 1 : pageNum + offset > endPage ? endPage : pageNum + offset;
-        if(newPage!==pageNum){
-            this.renderPage(newPage)
-            this.setState({ pageNum: newPage })
-        }
-    }
-
-    componentDidMount() {
-        this.loadPDF()
-    }
-    render() {
-        return (
-            <>
-                <Header style={{ background: "#fff" }}>
-                    <h2>PDF tytuł</h2>
-                </Header>
-                <Content >
-                    <Row type="flex" justify={'center'}>
-                        <Col xs={7}>
-                            <canvas className="pdf-window" />
-                        </Col>
-                    </Row>
-                    <Row type="flex" justify={'center'}>
-                        <Col xs={7}>
-                            <Row type="flex" justify={'center'}>
-                                <Col xs={12}>
-                                    <Button type="primary" onClick={() => this.changePage(-1)} icon='left'>
-                                        Poprzednia
-                                    </Button>
-                                </Col>
-
-                                <Col xs={12}>
-                                    <Button type="primary" onClick={() => this.changePage(1)} icon='right'>
-                                        Następna
-                                    </Button>
-
-                                </Col>
-                            </Row>
-
-                        </Col>
-
-                    </Row>
-                </Content>
-            </>
-        )
-    }
+                <Col xs={12}>
+                  <Button
+                    type="primary"
+                    onClick={() => this.changePage(1)}
+                    icon="right"
+                  >
+                    Następna
+                  </Button>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Content>
+      </>
+    );
+  }
 }
